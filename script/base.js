d20fe6f7b08a7ea139370cbccb712fe196951457
qnezor <qnezoru@gmail.com>
2023-07-09 17:07:06 +0300

    1

---
+++ script/base.js
@@ -0,0 +1,277 @@
+//база hfa
+
+const hfa = [
+
+    {
+        songname: 'Button',
+        artistname: 'PENGUIN RESEARCH',
+        music: '1PT61xh_gmy_WQ9xiWe8dCNhtR7NqDf3h',
+        cover: 'files/hfa/covers/button.jpg',
+    },
+    {
+        songname: 'Ready to',
+        artistname: 'Michiru Kagemori',
+        music: '146QDOz94vGmQmM4prDFZGYgEbw-uz55n',
+        cover: 'files/hfa/covers/readyto.jpg',
+    },
+    {
+        songname: 'セイシュンゼミナール',
+        artistname: 'Study',
+        music: '1-PPR7KKs1FwjfczuIfsh7oXjQh0rtR7G',
+        cover: 'files/hfa/covers/study.jpg',
+    },
+    {
+        songname: 'Can now, Can now',
+        artistname: 'Study',
+        music: '1HGUdR-lZqJS-MYuo5NkLMFXuohwEwJq7',
+        cover: 'files/hfa/covers/cannow.jpg',
+    },
+    {
+        songname: 'Houkagono Liberty',
+        artistname: 'halca',
+        music: '1UNUkLkiXfjdGlMN4cdncr6HY_hyCmyuv',
+        cover: 'files/hfa/covers/houkagonoliberty.jpg',
+    },
+    {
+        songname: 'Kyoumu Densen',
+        artistname: 'ALI PROJECT',
+        music: '1UlfwohoO0B14ldxXDsDQUojSBVT4hN3u',
+        cover: 'files/hfa/covers/kyoumudensen.jpg',
+    },
+    {
+        songname: 'Endless happy world',
+        artistname: 'Daisuke Ono',
+        music: '1Qd-oaGPiOlKmxcW7OctJzM7DhO0XpzNB',
+        cover: 'files/hfa/covers/endlesshappyworld.jpg',
+    },
+    {
+        songname: '徒花ネクロマンシー',
+        artistname: 'FranChouChou',
+        music: '1jFoKo08JKK94qkkkY-_RPHtX6pveYz_I',
+        cover: 'files/hfa/covers/fran.jpg',
+    },
+    {
+        songname: '光へ',
+        artistname: 'FranChouChou',
+        music: '1R9q7FCWNOc_REJ61JkTSzYXMTmETg4wt',
+        cover: 'files/hfa/covers/hikarie.jpg',
+    },
+    {
+        songname: 'Kago no Naka ni Tori',
+        artistname: 'yourness',
+        music: '1xAcwAfBFoQ4NV2PhywJR4pSXgJ15ugTi',
+        cover: 'files/hfa/covers/kagononakanitori.jpg',
+    },
+    {
+        songname: 'Aoibridge',
+        artistname: 'Sayuri',
+        music: '1NPf1uA8TRabLdfeq4Nutp_gFSSEmYFjG',
+        cover: 'files/hfa/covers/aoibridge.jpg',
+    },
+    {
+        songname: 'イエスタディをうたって',
+        artistname: 'agehasprings feat あに',
+        music: '1rGVCNWHWs57pj-o28U4lY5l6YsG2IFrr',
+        cover: 'files/hfa/covers/!nocover.jpg',
+    },
+    {
+        songname: 'Zen Zen Zense (Movie Version)',
+        artistname: 'RADWIMPS',
+        music: '1jmLjYozEt7wVOfdxhEl81qX-XdWFSc2w',
+        cover: 'files/hfa/covers/zenzenzense.jpg',
+    },
+    {
+        songname: 'なだめスかし Negotiation',
+        artistname: '鹿乃',
+        music: '1HxP6Ze2j_6DusvfZHA1KdGNR9d7tLUXu',
+        cover: 'files/hfa/covers/nadamesukashinegotiation.jpg',
+    },
+    {
+        songname: 'Kokoro Knock',
+        artistname: 'YuNi',
+        music: '1_A3WJuTvPez7rWB9YKzxnngVqnYBj4HY',
+        cover: 'files/hfa/covers/kokoroknock.jpg',
+    },
+    {
+        songname: 'Centimeter',
+        artistname: 'the peggies',
+        music: '1_mqPYkEcbNzFyj645chn9yYkDziJGUu4',
+        cover: 'files/hfa/covers/centimeter.jpg',
+    },
+    {
+        songname: 'Kokuhaku bungee jump',
+        artistname: 'halca',
+        music: '1Qkb4JXRIUGCnkhHmMfG2N832mUwSzCcX',
+        cover: 'files/hfa/covers/kokuhakubungeejump.jpg',
+    },
+    {
+        songname: 'Starting Now!',
+        artistname: 'Nana Mizuki',
+        music: '1q8vGJ0tplSntjJsezr32OMy-jfjVK-mg',
+        cover: 'files/hfa/covers/startingnow.jpg',
+    },
+    {
+        songname: 'Re:Re:',
+        artistname: 'ASIAN KUNG-FU GENERATION',
+        music: '1b_gSX_r_mNwR5Y7D3CxC3n9uZ4R3Cauo',
+        cover: 'files/hfa/covers/rere.jpg',
+    },
+    {
+        songname: 'ZENRYOKU SUMMER',
+        artistname: 'angela',
+        music: '1dGezt6SN8j3_Nhs2F6NEh9NEL9AFN6c3',
+        cover: 'files/hfa/covers/zenryokusummer.jpg',
+    },
+    {
+        songname: '恋のうた',
+        artistname: 'Yunomi',
+        music: '1BXJ9QxyQzwAe7AG1t6ckHMkXNrluzdG4',
+        cover: 'files/hfa/covers/yunomi.jpg',
+    },
+    {
+        songname: 'Kiminosei',
+        artistname: 'the peggies',
+        music: '18gZDv56ESn0MkAWQ_7bvL7smZdeQnejb',
+        cover: 'files/hfa/covers/kiminosei.jpg',
+    },
+    {
+        songname: 'Fukashigi no Carte',
+        artistname: '...',
+        music: '1WrgTzMbrt8OLWqWrNiOB7Frq9_I-xNPf',
+        cover: 'files/hfa/covers/fukashiginocarte.jpg',
+    },
+    {
+        songname: 'ココハドコ',
+        artistname: 'あほむし',
+        music: '1y8DWTzV-267GrSSFd-0QFIBfrooZ69OX',
+        cover: 'files/hfa/covers/mao.jpg',
+    },
+    {
+        songname: 'Kawakiwoameku',
+        artistname: 'Minami',
+        music: '1ICMGcMhwkUEtrzcUvD02KVW6U232_-h4',
+        cover: 'files/hfa/covers/kawakiwoameku.jpg',
+    },
+    {
+        songname: 'うたたねサンシャイン',
+        artistname: 'Unlimited tone',
+        music: '10ka2o9wyTxZr4pll6EQqC-Ub2BEi07qQ',
+        cover: 'files/hfa/covers/unlim.jpg',
+    },
+    {
+        songname: 'BON-BON',
+        artistname: 'CooRie',
+        music: '1FKYeunCsn0ww-RwxQ8hWwrjFCHdqthmr',
+        cover: 'files/hfa/covers/bonbon.jpg',
+    },
+    {
+        songname: 'Brand-new',
+        artistname: 'KANA-BOON',
+        music: '15102_w77LFloglzf2XBaHXc0rclPaeZP',
+        cover: 'files/hfa/covers/brandnew.jpg',
+    },
+    {
+        songname: 'Stand by Me',
+        artistname: 'the peggies',
+        music: '1MloOeefOl5kpxwduqtGA_ht42oxfGxLu',
+        cover: 'files/hfa/covers/standbyme.jpg',
+    },
+    {
+        songname: 'ワガママMIRROR HEART',
+        artistname: 'Ayaka Ohashi',
+        music: '1z28YPpJKX0QvT0VCyLfYp3jpIIoq444F',
+        cover: 'files/hfa/covers/mirrorheart.jpg',
+    },
+    {
+        songname: 'Garnet',
+        artistname: 'Hanako Oku',
+        music: '18kjP_QHiIBJri8UXxSE10Q0MwggNm2yp',
+        cover: 'files/hfa/covers/garnet.jpg',
+    },
+    {
+        songname: 'Fiction',
+        artistname: 'sumika',
+        music: '1pvPvusKRFJLhxI9cmds4R_sTHKzacH8B',
+        cover: 'files/hfa/covers/fiction.jpg',
+    },
+    {
+        songname: 'Kimi No Tonari',
+        artistname: 'halca',
+        music: '1jayjmDUt8sft7rRoePUTIUuX-r_di53i',
+        cover: 'files/hfa/covers/kiminotonari.jpg',
+    },
+    {
+        songname: 'SEE YOU',
+        artistname: 'Yuya Matsushita',
+        music: '1gHMo_zKZG1O--g-pkk4Ubg0ABfX6Wm9A',
+        cover: 'files/hfa/covers/seeyou.jpg',
+    },
+    {
+        songname: '巣立ちの歌',
+        artistname: 'アネモネリア',
+        music: '18JUr7YnMaUgwqcWx1_ivQJq8s7b-F7cv',
+        cover: 'files/hfa/covers/hz1.jpg',
+    },
+    {
+        songname: 'Life is サイダー',
+        artistname: 'アネモネリア',
+        music: '19TQGcmr4lkWMi9D7hpvQbc6CQgtesmQK',
+        cover: 'files/hfa/covers/lifeis.jpg',
+    },
+    {
+        songname: 'お後がよろしくって・・・よ!',
+        artistname: '極♨落女会',
+        music: '1Fk7Lb28hZvdhhVxQh8G0SIaf_rrkt9Wf',
+        cover: 'files/hfa/covers/hz2.jpg',
+    },
+    {
+        songname: 'x',
+        artistname: 'x',
+        music: 'x',
+        cover: 'files/hfa/covers/x.jpg',
+    },
+    {
+        songname: 'パズル',
+        artistname: 'ROUND TABLE feat. Nino',
+        music: '1ikiYg19ZfQdYsIu9LKEYjrGNCXmfYCBj',
+    },
+    {
+        songname: 'lost in the white',
+        artistname: '井澤愛',
+        music: '19wCvKQbtxkzXSqKNpwm9FRzdd3yke9nS',
+    },
+    {
+        songname: 'Dearest',
+        artistname: '三重野瞳',
+        music: '1a4Drzjs6krQWVz1z4iahYMfdzMno7y-o',
+    },
+    {
+        songname: 'Fukashigi no Carte',
+        artistname: '桜島麻衣、古賀朋絵、双葉理央、豊浜のどか、梓川かえで、牧之原翔子',
+        music: '1WrgTzMbrt8OLWqWrNiOB7Frq9_I-xNPf',
+        cover: 'files/hfa/covers/fukashiginocarte.jpg',
+    },
+    {
+        songname: 'x',
+        artistname: 'x',
+        music: 'x',
+        cover: 'files/hfa/covers/x.jpg',
+    },
+    {
+        songname: 'x',
+        artistname: 'x',
+        music: 'x',
+        cover: 'files/hfa/covers/x.jpg',
+    },
+    {
+        songname: 'x',
+        artistname: 'x',
+        music: 'x',
+        cover: 'files/hfa/covers/x.jpg',
+    },
+];
+
+let trackindex = 36;
+
+
+
---
+++ script/player.js
@@ -0,0 +1,169 @@
+const player = document.getElementById('player');
+
+var icon = document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]');
+
+// full player elements
+const songname_fullplayer = document.getElementById('songname_fullplayer');
+const artistname_fullplayer = document.getElementById('artistname_fullplayer');
+const cover_fullplayer = document.getElementById('cover_fullplayer');
+const background_fullplayer = document.getElementById('background_fullplayer');
+
+const nextsong_fullplayer = document.getElementById('nextsong_fullplayer');
+
+// mini player elements
+const songname_miniplayer = document.getElementById('songname_miniplayer');
+const artistname_miniplayer = document.getElementById('artistname_miniplayer');
+const cover_miniplayer = document.getElementById('cover_miniplayer');
+const background_miniplayer = document.getElementById('background_miniplayer');
+
+var perem = 'pause';
+
+let volume = document.getElementById('volume');
+
+// timeline
+let seek_slider = document.querySelector('.seek_slider');
+let curr_time = document.querySelector('.current-time');
+let total_duration = document.querySelector('.total-duration');
+
+let updateTimer;
+
+player.volume = volume.value / 100;
+
+function setVolume() {
+    player.volume = volume.value / 100;
+}
+
+function playpause() {
+    if (songname_fullplayer.textContent === "⠀") {
+        playerstart();
+    } else {
+        if (perem === 'pause') {
+            player.pause();
+            perem = 'play';
+            devpause();
+        } else {
+            player.play();
+            perem = 'pause';
+            devplay();
+        }
+    }
+}
+
+function devplay() {
+    document.getElementById('pp').src = "files/hfa/buttons/player_play.svg";
+    document.getElementById('pp1').src = "files/hfa/buttons/player_play.svg";
+}
+
+function devpause() {
+    document.getElementById('pp').src = "files/hfa/buttons/player_pause.svg";
+    document.getElementById('pp1').src = "files/hfa/buttons/player_pause.svg";
+}
+
+function nextTrack() {
+    if (trackindex === 0) {
+        trackindex = 36;
+    } else {
+        trackindex = trackindex - 1;
+        devplay();
+    }
+    playerstart();
+}
+
+function prevTrack() {
+    if (trackindex > 35) {
+        trackindex = 0;
+    } else {
+        trackindex = trackindex + 1;
+        devplay();
+    }
+    playerstart();
+}
+
+function openplayer() {
+    document.getElementById('miniplayer').style.opacity = "0";
+    document.getElementById('miniplayer').style.zIndex = "-1";
+    document.getElementById('fullplayer').style.opacity = "1";
+    document.getElementById('fullplayer').style.zIndex = "2";
+    document.getElementById('fullplayer').style.height = "100%";
+    document.getElementById('body').scroll = "no";
+    document.getElementById('body').style.overflow = "hidden";
+}
+
+function closeplayer() {
+    document.getElementById('miniplayer').style.opacity = "1";
+    document.getElementById('miniplayer').style.zIndex = "1";
+    document.getElementById('fullplayer').style.opacity = "0";
+    document.getElementById('fullplayer').style.zIndex = "-1";
+    document.getElementById('fullplayer').style.height = "0";
+    document.getElementById('body').scroll = "yes";
+    document.getElementById('body').style.overflow = "auto";
+}
+
+// timeline script
+function seekTo() {
+    let seekto = player.duration * (seek_slider.value / 100);
+    player.currentTime = seekto;
+}
+
+function setUpdate() {
+    let seekPosition = 0;
+    if (!isNaN(player.duration)) {
+        seekPosition = player.currentTime * (100 / player.duration);
+        seek_slider.value = seekPosition;
+
+        let currentMinutes = Math.floor(player.currentTime / 60);
+        let currentSeconds = Math.floor(player.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(player.duration / 60);
+        let durationSeconds = Math.floor(player.duration - durationMinutes * 60);
+
+        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
+    }
+}
+
+// player play/update
+
+function playerstart() {
+    clearInterval(updateTimer);
+    curr_time.textContent = "00:00";
+    total_duration.textContent = "00:00";
+    seek_slider.value = 0;
+    devplay();
+    player.src = "https://drive.google.com/uc?export=download&id=" + hfa[trackindex].music;
+    player.load();
+    playerupdate();
+    updateTimer = setInterval(setUpdate, 1000);
+    player.addEventListener('ended', nextTrack);
+}
+
+function playerupdate() {
+    icon.forEach(function (element) {
+        element.setAttribute('href', hfa[trackindex].cover);
+    });
+    document.title = hfa[trackindex].songname;
+    // full player
+    songname_fullplayer.textContent = hfa[trackindex].songname;
+    artistname_fullplayer.textContent = hfa[trackindex].artistname;
+
+    if (trackindex === 0) {
+        nextsong_fullplayer.textContent = "Next Song: " + hfa[36].songname
+    } else {
+        nextsong_fullplayer.textContent = "Next Song: " + hfa[trackindex - 1].songname
+    }
+
+    cover_fullplayer.src = hfa[trackindex].cover;
+    background_fullplayer.style.backgroundImage = "url(" + hfa[trackindex].cover + ")";
+
+    // mini player
+    songname_miniplayer.textContent = hfa[trackindex].songname;
+    artistname_miniplayer.textContent = hfa[trackindex].artistname;
+
+    cover_miniplayer.src = hfa[trackindex].cover;
+    background_miniplayer.style.backgroundImage = "url(" + hfa[trackindex].cover + ")";
+
+}
